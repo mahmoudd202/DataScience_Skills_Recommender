2d5ec510e70751d8d1961e3c21e9b645a872d8bd
mahmouddd202 <166512279+mahmouddd202@users.noreply.github.com>
2025-05-15 14:41:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -635,4 +635,4 @@ def create_interface():
 
 if __name__ == "__main__":
     demo = create_interface()
-    demo.launch(server_name="0.0.0.0", server_port=10000)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
